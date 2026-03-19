85a12177fcf004fbe93aec30a6519a5d1ae2f67e
Chocolatey-AU <chocolatey@realdimensions.net>
2026-03-19 00:36:38 +0000

    AU: 1 updated - azcopy10 [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/f2ea11e48752860ccae3c7a95697e89cd0f0420a

--- automatic/azcopy10/tools/chocolateyInstall.ps1
+++ automatic/azcopy10/tools/chocolateyInstall.ps1
@@ -5,11 +5,11 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
     packageName    = $packageName
-    url64          = 'https://github.com/Azure/azure-storage-azcopy/releases/download/v10.32.1/azcopy_windows_amd64_10.32.1.zip'
-    checksum64     = '0da130ff3772c8549784b6813106177c5b60fef550aee640268b93d50551aa9a'
+    url64          = 'https://github.com/Azure/azure-storage-azcopy/releases/download/v10.32.2/azcopy_windows_amd64_10.32.2.zip'
+    checksum64     = '83c71e16cd12ed7e5f2d63d041549d1de8718e1c81e2dbf4bcbe5fcaafe2aab9'
     checksumType64 = 'sha256'
-    url            = 'https://github.com/Azure/azure-storage-azcopy/releases/download/v10.32.1/azcopy_windows_386_10.32.1.zip'
-    checksum       = '8f2158dd7f096d98c98401f5acd2e6e5a3cfaacc38089187d8f78456589956df'
+    url            = 'https://github.com/Azure/azure-storage-azcopy/releases/download/v10.32.2/azcopy_windows_386_10.32.2.zip'
+    checksum       = 'a4dbc8e3201f6dc0bc4de091f2356979dc5808622e505856e9fc2bf796cd6650'
     checksumType   = 'sha256'
     destination    = $toolsDir
 }
