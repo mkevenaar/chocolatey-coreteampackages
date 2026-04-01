35fb54a015eb0b9007daaeed12c1cad0dbbb7908
Chocolatey-AU <chocolatey@realdimensions.net>
2026-04-01 00:36:36 +0000

    AU: 3 updated - chromium vim Waterfox [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/52fbe4adac871c1455570c2fd102bb507b919c79

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 . (Join-Path $toolsDir 'helper.ps1')
 
-$version = '148.0.7766.0-snapshots'
+$version = '148.0.7767.0-snapshots'
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
@@ -15,8 +15,8 @@ if (Test-Path $Chromium) {
 
 $packageArgs = @{
   packageName   = 'chromium'
-  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1607619/mini_installer.exe'
-  checksum      = 'C145ED2DC533E43444D2AD9F0FB399C5DDC2AD28E5C052D20553C3502A0F3DBF'
+  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1608014/mini_installer.exe'
+  checksum      = '2624360B6CE0AAC0CC3BC4C1F4EC87636240A335FD9DD2C7CBC0E16B3095FBB7'
   checksumType  = 'sha256'
   file64        = "$toolsdir\chromium_x64.exe"
   fileType      = 'exe'
--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_9.2.0272_x86.zip"
-  file64        = "$toolsDir\gvim_9.2.0272_x64.zip"
+  file          = "$toolsDir\gvim_9.2.0276_x86.zip"
+  file64        = "$toolsDir\gvim_9.2.0276_x64.zip"
 }
 
 $installArgs = @{
