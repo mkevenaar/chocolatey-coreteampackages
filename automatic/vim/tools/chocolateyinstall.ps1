c064af1fc01d162153c15ae2549a5fb448e254b1
Chocolatey-AU <chocolatey@realdimensions.net>
2026-04-23 00:37:01 +0000

    AU: 2 updated - openshift-cli vim [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/e5e565b1f0e96d8094e4120391c59dc00b929461

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_9.2.0387_x86.zip"
-  file64        = "$toolsDir\gvim_9.2.0387_x64.zip"
+  file          = "$toolsDir\gvim_9.2.0389_x86.zip"
+  file64        = "$toolsDir\gvim_9.2.0389_x64.zip"
 }
 
 $installArgs = @{
