c7f009014066eabe2d984c73e9767fec5c99311a
Chocolatey-AU <chocolatey@realdimensions.net>
2026-04-28 06:38:53 +0000

    AU: 3 updated - activepresenter chromium djv [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/f5b183a1ba779f12acc0fc5df37b6b68f41fe62a

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 . (Join-Path $toolsDir 'helper.ps1')
 
-$version = '149.0.7813.0-snapshots'
+$version = '149.0.7814.0-snapshots'
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
@@ -15,8 +15,8 @@ if (Test-Path $Chromium) {
 
 $packageArgs = @{
   packageName   = 'chromium'
-  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1621297/mini_installer.exe'
-  checksum      = '590B3A7C10E639953D047CB3BA60E580C50801556B4B3269C58DC9236D527C95'
+  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1621507/mini_installer.exe'
+  checksum      = '197582EF2641CF7E4C793182741008D424DFFEA92E4DB2EB5E14A415AB7FE0DF'
   checksumType  = 'sha256'
   file64        = "$toolsdir\chromium_x64.exe"
   fileType      = 'exe'
